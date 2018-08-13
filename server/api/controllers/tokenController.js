8e7fc60768de0596796c919fd20f2b265d02f728
falahudinhs <falahudin6@gmail.com>
2018-08-13 15:17:03 +0700

    user approval

--- server/api/controllers/tokenController.js
+++ server/api/controllers/tokenController.js
@@ -3,10 +3,8 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try {
         var token = req.headers.authorization.split(" ")[1];
-        console.log(token);
         var decode = jwt.verify(token, "secretkey");
         req.userData = decode; //
-        console.log(req.userData)
         next(); // success auth
     } catch (error) {
         return res.status(401).json({
